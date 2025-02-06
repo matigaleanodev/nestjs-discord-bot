6513589a3fb34ad1aa1dfcfdfb9ec50f2ba774a2
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-02-06 17:16:51 -0300

    feat: reemplazo del proyecto manteniendo historial

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BotModule } from './bot/bot.module';
-import * as dotenv from 'dotenv';
-import { ConfigModule } from '@nestjs/config';
-
-dotenv.config();
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    BotModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
