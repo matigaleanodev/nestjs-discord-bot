5c3e85a914b5bc8bc939e3f79c397617b9ecc7aa
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-02-13 23:27:14 -0300

    cookies para youtube

--- src/bot/services/youtube/youtube.service.ts
+++ src/bot/services/youtube/youtube.service.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import * as ytdl from '@distube/ytdl-core';
 import * as ytsr from '@distube/ytsr';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class YoutubeService {
+  private cache = new Map<
+    string,
+    { info: ytdl.videoInfo; stream: ReturnType<typeof ytdl.downloadFromInfo> }
+  >();
+  private readonly cookies: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.cookies = this.configService.get<string>('YOUTUBE_COOKIES') || '';
+  }
+
   async getSong(query: string) {
     console.log('Buscando:', query);
     const isValidUrl = ytdl.validateURL(query);
 
     try {
       let videoUrl = query;
+
+      if (this.cache.has(query)) {
+        console.log('⚡ Resultado desde caché');
+        return this.cache.get(query) as {
+          info: ytdl.videoInfo;
+          stream: ReturnType<typeof ytdl.downloadFromInfo>;
+        };
+      }
 
       if (!isValidUrl) {
         const result = await ytsr(query, { limit: 1, type: 'video' });
@@ -18,7 +37,14 @@ export class YoutubeService {
         videoUrl = video.url;
       }
 
-      const info = await ytdl.getBasicInfo(videoUrl);
+      const requestOptions = {
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+          Cookie: this.cookies,
+        },
+      };
+
+      const info = await ytdl.getBasicInfo(videoUrl, { requestOptions });
 
       const stream = ytdl(videoUrl, {
         filter: 'audioonly',
@@ -26,9 +52,15 @@ export class YoutubeService {
         highWaterMark: 1 << 25,
         dlChunkSize: 0,
         liveBuffer: 20000,
+        requestOptions,
       });
 
-      return { info, stream };
+      const result = { info, stream };
+
+      this.cache.set(query, result);
+      setTimeout(() => this.cache.delete(query), 10 * 60 * 1000);
+
+      return result;
     } catch (error) {
       console.error('Error al obtener el video:', error);
       throw new Error('No se pudo obtener el video');
